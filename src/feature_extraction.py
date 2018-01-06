ad370b8dba49d85ff7c59a30682ae69252a61b46
Hermes Martinez <Hermes@Narwhal.local>
2018-01-06 20:17:25 +0100

    feature creation updated

---
+++ src/feature_extraction.py
@@ -0,0 +1,325 @@
+# coding: UTF-8
+"""
+Verb-sense disambiguation demonstration
+"""
+from corpus_reading import conll_list
+
+
+def feature_extraction(example_list, target_verb, window=5):
+    """
+    Feature extraction
+    """
+
+    # Sub lists of feats
+    target_verb_index = []
+    window_word_feats = []
+    base_surrounding_word_feats = []
+
+    raw_features = complex_feature_extraction(example_list, target_verb)
+
+    # target_verb_index = get_target_verb_i(example_list, target_verb)
+
+    # finding index of target_verb for each example
+    for exemple in example_list:
+        for word_tags in exemple:
+            if word_tags[2] == target_verb:
+                target_verb_index.append(word_tags[0])
+                break
+
+    # Extracts the linear context of the target verb (words to the left and to the right)
+    window_word_feats = word_window(example_list, target_verb, window)
+    base_surrounding_word_feats = base_surrounding_words(
+        example_list, target_verb, window)
+
+    # Insert the 3 first basic features at the beginning of the list of features
+    raw_features.insert(0, base_surrounding_word_feats)
+    raw_features.insert(0, window_word_feats)
+    raw_features.insert(0, target_verb_index)
+
+    return raw_features
+
+
+def word_window(example_list, target_verb, window=5):
+    """
+    Extract the words surrounding the target verb WITH padding
+    """
+    ww_feautures = []
+    beg = "@@@"
+    end = "$$$"
+    position = 0
+    beg_idx = 0
+    end_idx = 0
+    c = 0
+    for example in example_list:
+        c += 1
+        ww_example = []
+        for word_elements in example:
+            if word_elements[2] == target_verb:
+                position = int(word_elements[0]) - 1
+        beg_idx = position - window
+        if beg_idx < 0:
+            for idx in range(beg_idx, 0):
+                ww_example.append(beg)
+            beg_idx = 0
+        end_idx = position + window + 1
+        padding = 0
+        if end_idx > len(example):
+            padding = end_idx - len(example) + 1
+            end_idx = len(example)
+        for idx in range(beg_idx, end_idx):
+            if idx == position:
+                continue
+            # print(idx)
+            ww_example.append(example[idx][1])
+        if padding > 0:
+            for idx in range(padding - 1):
+                ww_example.append(end)
+        # ww_example.append(position + 1)
+        ww_feautures.append(ww_example)
+    return ww_feautures
+
+
+def base_surrounding_words(example_list, target_verb, window=5):
+    """
+    Extract the words surrounding the target verbs (WITHOUT PADDING)
+    """
+    ww_feautures = []
+    position = 0
+    beg_idx = 0
+    end_idx = 0
+    for example in example_list:
+        ww_example = []
+        for word_elements in example:
+            if word_elements[2] == target_verb:
+                position = int(word_elements[0]) - 1
+        beg_idx = max(position - window, 0)
+        end_idx = min(position + window + 1, len(example))
+        for idx in range(beg_idx, end_idx):
+            ww_example.append(example[idx][1])
+        ww_feautures.append(ww_example)
+    return ww_feautures
+
+
+def y_senseId_extraction(example_list, target_verb):
+    """
+    Extract the Y values, that is to say a list of IDs of the sense for each example
+    ARGUMENTS : list of sentences (corpus), the target verb (aborder,abbattre or affecter)
+    RETURN : a list of the IDs of senses for each sentence in the corpus
+    """
+    y_senseID = []
+
+    for example in example_list:
+        for word_elements in example:
+            if word_elements[2] == target_verb:  # Target verb information
+                # list of information in column 5
+                info_list = word_elements[5].split('|')
+                for e in info_list:
+                    if "sense=" in e:  # information obout the sense
+                        sense_list = e.split('#')
+                        # we add the sense ID of the current example into the big list with all the examples
+                        y_senseID.append(sense_list[1])
+                break  # there is one example were there are 2 occurrences of the target verb in one sentence, we chose to select the first one only
+
+    return y_senseID
+
+
+def complex_feature_extraction(example_list, target_verb):
+    """
+    Gets the informations about the surface syntaxic context of the target verb (word forms and tags) and its deep syntaxic
+    context (word forms and tags)
+    INPUT: the list of examples (corpus), the target verb
+    RETURN: 8 lists
+    """
+
+    # LIST FOR THE WHOLE CORPUS (all the examples)
+    surf_dep_words = []
+    surf_dep_tags = []
+    surf_gov_words = []
+    surf_gov_tags = []
+
+    deep_dep_words = []
+    deep_dep_tags = []
+    deep_gov_words = []
+    deep_gov_tags = []
+
+    position = 0
+    count = 0
+    # We go through the list of examples(sentences)
+    for example in example_list:
+        count += 1
+
+        # LISTS FOR THE CURRENT EXAMPLE (the current sentence)
+        curr_surf_dep_words = []
+        curr_surf_dep_tags = []
+        curr_surf_gov_words = []
+        curr_surf_gov_tags = []
+
+        curr_deep_dep_words = []
+        curr_deep_dep_tags = []
+        curr_deep_gov_words = []
+        curr_deep_gov_tags = []
+
+        gov_idx_surf = []
+        gov_idx_deep = []
+
+        # Target verb position finding loop
+        for word_elements in example:
+            # IF WORDFORM EQUALS TARGET VERB, WE'VE FOUND ITS POSITION
+            if word_elements[2] == target_verb:
+                position = int(word_elements[0])
+                break  # there is one example were there are 2 occurrences of the target verb in one sentence, we chose to select the first one only
+
+        for word_elements in example:  # we go through each word in the sentence
+
+            # index of words that are connected to the current word
+            target_idx_list = word_elements[6].split('|')
+            # tags of words that are connected to the current word
+            target_tag_list = word_elements[7].split('|')
+
+            for i in range(len(target_idx_list)):
+
+                # Check to find DEPENDANTS: (that is to say, words that "points" to the target verb index)
+
+                if int(position) == int(target_idx_list[i]):
+                        # Surface syntaxe
+                    if "S:" in target_tag_list[i]:
+                        curr_surf_dep_words.append(word_elements[1])
+                        curr_surf_dep_tags.append(target_tag_list[i][2:])
+                    # Deep syntaxe
+                    elif "D:" in target_tag_list[i]:
+                        curr_deep_dep_words.append(word_elements[1])
+                        curr_deep_dep_tags.append(target_tag_list[i][2:])
+                    # if there is no 'S' or 'D', it is for both types of syntaxe,we add into the two lists
+                    else:
+                        curr_surf_dep_words.append(word_elements[1])
+                        curr_surf_dep_tags.append(target_tag_list[i])
+                        curr_deep_dep_words.append(word_elements[1])
+                        curr_deep_dep_tags.append(target_tag_list[i])
+
+            # Check to find GOVERNORS: (that is to say, the word indexes the target verb "points" to)
+
+            # If it's is the target verb
+            if int(position) == int(word_elements[0]):
+                for i in range(len(target_idx_list)):
+                        # Surface syntaxe
+                    if "S:" in target_tag_list[i]:
+                        gov_idx_surf.append(target_idx_list[i])
+                        curr_surf_gov_tags.append(target_tag_list[i][2:])
+                    # Deep syntaxe
+                    elif "D:" in target_tag_list[i]:
+                        gov_idx_deep.append(target_idx_list[i])
+                        curr_deep_gov_tags.append(target_tag_list[i][2:])
+                    # if there is no 'S' or 'D', it is for both types of syntaxe,we add into the two lists
+                    else:
+                        gov_idx_surf.append(target_idx_list[i])
+                        gov_idx_deep.append(target_idx_list[i])
+                        curr_surf_gov_tags.append(target_tag_list[i])
+                        curr_deep_gov_tags.append(target_tag_list[i])
+
+                # now that we have the indexes, we need to get the word forms
+                for i in gov_idx_surf:
+                    if int(i) == 0:
+                        # Append "root" if i == 0
+                        curr_surf_gov_words.append(example[position - 1][7])
+                    else:
+                        curr_surf_gov_words.append(example[int(i) - 1][1])
+                for i in gov_idx_deep:
+                    if int(i) == 0:
+                        # Append "root" if i == 0
+                        curr_deep_gov_words.append(example[position - 1][7])
+                    else:
+                        curr_deep_gov_words.append(example[int(i) - 1][1])
+
+        # We append the information of the current example in the big list with the informations for all the examples
+        surf_dep_words.append(curr_surf_dep_words)
+        surf_dep_tags.append(curr_surf_dep_tags)
+        surf_gov_words.append(curr_surf_gov_words)
+        surf_gov_tags.append(curr_surf_gov_tags)
+
+        deep_dep_words.append(curr_deep_dep_words)
+        deep_dep_tags.append(curr_deep_dep_tags)
+        deep_gov_words.append(curr_deep_gov_words)
+        deep_gov_tags.append(curr_deep_gov_tags)
+
+    return [surf_dep_words,
+            surf_dep_tags,
+            surf_gov_words,
+            surf_gov_tags,
+
+            deep_dep_words,
+            deep_dep_tags,
+            deep_gov_words,
+            deep_gov_tags]
+
+
+def obtain_examples(file):
+    """
+    Obtains a list from a connll file
+    """
+    example_list = conll_list(file)
+    return example_list
+
+
+CONLL = "data_WSD_VS/aborder.deep_and_surf.sensetagged.conll"
+ex = obtain_examples(CONLL)
+RAW_F = feature_extraction(ex, "aborder")
+
+"""
+RAW FEATURES :
+
+ POSITION OF THE TARGET VERB:
+ 	0  target verb index
+
+ LINEAR CONTEXT:
+ 	1  window words features WITH padding
+ 	2  base_surrounding_featsWITHOUT padding
+
+ SURFACE SYNTAXE CONTEXT:
+ 	3  dependant words
+ 	4  dependant tags
+ 	5  governor words
+ 	6  governor tags
+
+ DEEP SYNTAXE CONTEXTE
+ 	7  dependant words
+ 	8  dependant tags
+ 	9  governor words
+ 	10 governor tags
+"""
+
+# print("---------------------------------------------------------------------------------------------------")
+# print("EXAMPLE : aborder, 219")
+# print("---------------------------------------------------------------------------------------------------")
+# print("target verb position: ", RAW_F[0][10])
+# print("")
+# print("LINEAR CONTEXT:")
+# print(RAW_F[1][10])
+# print(RAW_F[2][10])
+# print("")
+# print("SURFACE SYNTAXE CONTEXT:")
+# print(RAW_F[3][10])
+# print(RAW_F[4][10])
+# print(RAW_F[5][10])
+# print(RAW_F[6][10])
+# print("")
+# print("DEEP SYNTAXE CONTEXT:")
+# print(RAW_F[7][10])
+# print(RAW_F[8][10])
+# print(RAW_F[9][10])
+# print(RAW_F[10][10])
+# print("")
+# print("sense ID: ", y_senseId_extraction(ex, "aborder")[219])
+# print("")
+# print("---------------------------------------------------------------------------------------------------")
+# print("WHOLE LISTS")
+# print("---------------------------------------------------------------------------------------------------")
+# # print(RAW_F[2])
+# print("---------------------------------------------------------------------------------------------------")
+# # print(RAW_F[3])
+# print("---------------------------------------------------------------------------------------------------")
+# # print(RAW_F[4])
+# print("---------------------------------------------------------------------------------------------------")
+# # print(RAW_F[5])
+# print("---------------------------------------------------------------------------------------------------")
+# # print(RAW_F[6])
+# print("---------------------------------------------------------------------------------------------------")
---
+++ src/nn_vsd.py
@@ -0,0 +1,161 @@
+# coding: UTF-8
+"""
+NEURAL NETWORK MODEL
+"""
+import numpy as np
+from keras.utils import to_categorical
+from keras.preprocessing import text
+from corpus_reading import conll_list, read_embeddings
+from feature_extraction import feature_extraction
+
+
+def encode_features(raw_features, embeddings_file):
+    """
+    types: (list) -> list
+    structure: ("raw features") -> "encoded features"
+    """
+    encoded_features = []
+    embeddings_index = read_embeddings(embeddings_file)
+    # We obtain a list of possible tags for dependency trees
+
+    def cat_list(tag_list):
+        out = []
+        if isinstance(tag_list[0], list):
+            for sublist in tag_list:
+                for element in sublist:
+                    out.append(element)
+            return out
+        return tag_list
+
+    tag_cat = cat_list(raw_features[4]) + cat_list(raw_features[6]) + \
+        cat_list(raw_features[8]) + cat_list(raw_features[10])
+    # We create a vocabulary of dependency tags
+    tag_vocab = set(tag_cat)
+    vocab_list = list(tag_vocab)
+
+    print(len(raw_features))
+    print(len(raw_features[0]))
+    # First feature list corresponds to target verb position
+    # encoded to int
+    encoded_features.append(list(map(int, raw_features[0])))
+
+    # Second feature list corresponds to base surrounding words with padding
+    # encoded to word embeddings
+    encoded_features.append(embedding_encoding(
+        raw_features[1], embeddings_index))
+
+    # Third feature list corresponds to base surrounding words without padding
+    # including the target verb in the middle.
+    # encoded to word embeddings
+    encoded_features.append(embedding_encoding(
+        raw_features[2], embeddings_index))
+
+    # Fourth feature list corresponds to surface dependant words
+    # encoded to word embeddings
+    encoded_features.append(embedding_encoding(
+        raw_features[3], embeddings_index))
+
+    # Fifth feature list corresponds to surface dependant tags
+    # encoded to one hot vectors
+    encoded_features.append(one_hot_encoding(
+        raw_features[4], vocab_list))
+
+    # Sixth feature list corresponds to surface governor words
+    # econded to word embeddings
+    encoded_features.append(embedding_encoding(
+        raw_features[5], embeddings_index))
+
+    # Seventh feature list corresponds to surface governor tags
+    # encoded to one hot vectors
+    encoded_features.append(one_hot_encoding(
+        raw_features[6], vocab_list))
+
+    # Eighth feature list corresponds to deep dependant words
+    # encoded to word embeddings
+    encoded_features.append(embedding_encoding(
+        raw_features[7], embeddings_index))
+
+    # Ninth feature list corresponds to deep dependant tags
+    # encoded to one hot vectors
+    encoded_features.append(one_hot_encoding(
+        raw_features[8], vocab_list))
+
+    # Tenth feature list corresponds to deep governor words
+    # econded to word embeddings
+    encoded_features.append(embedding_encoding(
+        raw_features[9], embeddings_index))
+
+    # Eleventh feature list corresponds to deep governor tags
+    # encoded to one hot vectors
+    encoded_features.append(one_hot_encoding(
+        raw_features[10], vocab_list))
+    return encoded_features
+
+
+def embedding_encoding(word_list, embeddings_index):
+    """
+    types: (list) -> list
+    structure: ("raw features") -> "encoded features"
+    """
+    def context_mapping(word_list):
+        """
+        Embedding mapping sub func
+        """
+        emb_map = np.zeros(100)
+        wordc = 0
+        for word in word_list:
+            if word in embeddings_index:
+                wordc += 1
+                emb_map = np.add(emb_map, embeddings_index[word])
+        emb_map /= wordc
+        # emb_map = [embeddings_index[word] for word in word_list if word in embeddings_index]
+        return emb_map
+
+    context_embeddings = list(map(context_mapping, word_list))
+    return context_embeddings
+
+
+def one_hot_encoding(tag_list, vocab_list):
+    """
+    types: (list, list) -> list
+    structure: ("raw features") -> "encoded features"
+    """
+    enc_tag_list = []
+    for tags in tag_list:
+        enc_tags = []
+        for tag in tags:
+            enc_tag = text.hashing_trick(tag, len(
+                vocab_list), hash_function='md5', filters='!"#$%&()*+,-/;<=>?@[\\]^_`{|}~\t\n', lower=False)
+            enc_tags.append(enc_tag[0])
+        enc_tag_list.append(enc_tags)
+    # enc_tag_list = np.array(enc_tag_list)
+    bin_enc_tag_list = []
+    for enc_list in enc_tag_list:
+        bin_enc_tag_list.append(to_categorical(enc_list, len(vocab_list)))
+
+    return bin_enc_tag_list
+    # bin_enc_tag_list = list(map(to_categorical, enc_tag_list))
+    print(tag_list[0:5])
+    print(enc_tag_list[0:5])
+    print(bin_enc_tag_list[0:5])
+    print(len(enc_tag_list))
+    print(len(enc_tag_list[0:5]))
+    print(len(tag_list[0:5]))
+    exit(0)
+
+
+def obtain_examples(file):
+    """
+    Obtains a list from a connll file
+    """
+    example_list = conll_list(file)
+    return example_list
+
+
+CONLL = "data_WSD_VS/aborder.deep_and_surf.sensetagged.conll"
+WE = "data_WSD_VS/vecs100-linear-frwiki"
+ex = obtain_examples(CONLL)
+RAW_F = feature_extraction(ex, "aborder")
+ENC_F = encode_features(RAW_F, WE)
+for i in range(11):
+    print(ENC_F[i][0])
